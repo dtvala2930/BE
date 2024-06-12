89b9bb071ffae2809ec5c9d48928192593a519ff
Leo <dt.duytan1999@gmail.com>
2024-06-12 10:27:17 +0700

    feat(src): remove compression

--- src/main.ts
+++ src/main.ts
@@ -37,7 +37,6 @@ async function bootstrap() {
     }),
   );
 
-  // Apply middleware compression
   app.use(bodyParser.json({ limit: '2048mb' }));
   app.use(bodyParser.urlencoded({ limit: '2048mb', extended: true }));
 
